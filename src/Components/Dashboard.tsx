d6c1d6c128cd4a104d01a4a801ece022f65835b6
Saaibi Yesid Flores Hernandez <sflores@grupolpa.com>
2023-02-15 20:13:13 -0500

    formatDate

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -5,28 +5,30 @@ import { selectUser } from '../features/users/userSlice';
 import Hobbies from './Hobbies';
 
 export default function Dashboard() {
-    const user = useAppSelector(selectUser);
+    const { user } = useAppSelector(selectUser);
+
+    const formatDate = (date: any) => date && new Date(date).toISOString().split('T')[0];
 
     return (
         <div className="container text-center">
             <div className="row">
                 <div className="col">
                     <ul className="list-group">
-                        <li className="list-group-item">{user.user.firstName}</li>
-                        <li className="list-group-item">{user.user.lastName}</li>
-                        <li className="list-group-item">{user.user.email}</li>
-                        <li className="list-group-item">{`${user.user.phoneNumber}`}</li>
-                        <li className="list-group-item">{user.user.documentType}</li>
-                        <li className="list-group-item">{`${user.user.documentNumber}`}</li>
-                        <li className="list-group-item">{`${user.user.birthdate}`}</li>
-                        <li className="list-group-item">{`${user.user.expeditionDate}`}</li>
-                        <li className="list-group-item">{user.user.country}</li>
-                        <li className="list-group-item">{user.user.city}</li>
-                        <li className="list-group-item">{user.user.address}</li>
+                        <li className="list-group-item">{user.firstName}</li>
+                        <li className="list-group-item">{user.lastName}</li>
+                        <li className="list-group-item">{user.email}</li>
+                        <li className="list-group-item">{`${user.phoneNumber}`}</li>
+                        <li className="list-group-item">{user.documentType}</li>
+                        <li className="list-group-item">{`${user.documentNumber}`}</li>
+                        <li className="list-group-item">{`${formatDate(user.birthdate)}`}</li>
+                        <li className="list-group-item">{`${formatDate(user.expeditionDate)}`}</li>
+                        <li className="list-group-item">{user.country}</li>
+                        <li className="list-group-item">{user.city}</li>
+                        <li className="list-group-item">{user.address}</li>
                     </ul>
                 </div>
                 <div className="col">
-                    <UserEdit id={user.user._id} />
+                    <UserEdit id={user._id} />
                 </div>
             </div>
             <Hobbies />
