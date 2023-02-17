3aceb7745d3dbbebe734fbfdee7dcd51cb2bd974
Saaibi Yesid Flores Hernandez <sflores@grupolpa.com>
2023-02-17 08:07:04 -0500

    uploadImage

--- src/features/users/UserEdit.tsx
+++ src/features/users/UserEdit.tsx
@@ -6,13 +6,35 @@ type UserEdits = {
     id: string;
 };
 
+// type UserState {
+//     country: string,
+//     city: string,
+//     address: string,
+//     photoProfile: string,
+// }
+
 export default function UserEdit({ id }: UserEdits) {
     const dispatch = useAppDispatch();
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState<object>({});
 
     const userOnChange = (field: string, data: any) => {
         setUser((state: any) => ({ ...state, [field]: data, id }))
     }
+
+    const uploadImage = async (field: string, event: any) => {
+        // const file = event.target.files[0];
+        // userOnChange(field, file);
+    };
+
+    const onClickBtn = () => {
+        // const formData = new FormData();
+        // Object.keys(user).forEach((k) => {
+        //     formData.append(k, user[k])
+        // })
+        dispatch(editUser({ user, id })).unwrap()
+    }
+
+
     return (
         <>
             <div className="mb-3">
@@ -27,7 +49,11 @@ export default function UserEdit({ id }: UserEdits) {
                 <label htmlFor="address" className="form-label">Address</label>
                 <input id='address' onChange={(e) => userOnChange('address', e.target.value)} type="text" maxLength={100} className="form-control" />
             </div>
-            <button onClick={() => dispatch(editUser({ user, id })).unwrap()} className="btn btn-secondary me-3" type="button">Save</button>
+            <div className="mb-3">
+                <label htmlFor="formFile" className="form-label">Photo Profile</label>
+                <input className="form-control" accept="image/*" onChange={(e) => uploadImage('photoProfile', e)} type="file" id="formFile" />
+            </div>
+            <button onClick={onClickBtn} className="btn btn-secondary me-3" type="button">Save</button>
         </>
     )
 }
